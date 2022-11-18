c45e090007b230d68b28df7ac69139f032a8ff40
anapbds <justtley@gmail.com>
2022-11-17 23:36:44 -0300

    models

--- api/src/app/models/Order.ts
+++ api/src/app/models/Order.ts
@@ -11,6 +11,21 @@ export const Order = model('Order', new Schema({
     default: 'WAITING',
   },
   createAt: {
-    
-  }
+    type: Date,
+    default: Date.now,
+  },
+  products: {
+    required: true,
+    type: [{
+      product: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Product',
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+    }],
+  },
 }));
