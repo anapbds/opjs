c8a2ea5b6e635d21049dd911aa2d5e55c213efa5
anapbds <justtley@gmail.com>
2022-11-23 00:38:01 -0300

    use cases category

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,48 +1,46 @@
 import { Router } from 'express';
+import { createCategories } from './app/useCases/categories/createCategory';
+import { listCategories } from './app/useCases/categories/listCategories';
 
 export const router = Router();
 
 // List categories
-router.get('/categories', (req,res) => {
-  res.send('OK');
-});
+router.get('/categories', listCategories);
 
 // Create Category
-router.post('/categories', (req, res) => {
-  res.send('OK');
-});
+router.post('/categories', createCategories);
 
 // List Products
-router.get('/products', (req,res) => {
+router.get('/products', (req, res) => {
   res.send('OK');
 });
 
 // Create Product
-router.post('/products', (req,res) => {
+router.post('/products', (req, res) => {
   res.send('OK');
 });
 
 // Get products by category
-router.get('/categories/:categoryID/products', (req,res) => {
+router.get('/categories/:categoryID/products', (req, res) => {
   res.send('OK');
 });
 
 // List Orders
-router.get('/orders', (req,res) => {
+router.get('/orders', (req, res) => {
   res.send('OK');
 });
 
 // Create Order
-router.post('/orders', (req,res) => {
+router.post('/orders', (req, res) => {
   res.send('OK');
 });
 
 // Change order status
-router.patch('/orders/:orderID', (req,res) => { //patch = alteração parcial
+router.patch('/orders/:orderID', (req, res) => { //patch = alteração parcial
   res.send('OK');
 });
 
 // Delete/cancel order
-router.delete('/orders/:orderID', (req,res) => { 
+router.delete('/orders/:orderID', (req, res) => {
   res.send('OK');
 });
