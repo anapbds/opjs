f60f474b6b889940abfa1fed92777277d696b450
anapbds <justtley@gmail.com>
2022-11-25 12:00:16 -0300

    API finalizada

--- api/src/app/useCases/orders/changeOrderStatus.ts
+++ api/src/app/useCases/orders/changeOrderStatus.ts
@@ -7,12 +7,15 @@ export async function changeOrderStatus(req: Request, res: Response) {
     const { orderId } = req.params;
     const { status } = req.body;
 
-    if(['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)){
+    if(!['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)){
       return res.status(400).json({
-        error: 'Status should be one these: WAITING, IN_PRODUCTION, DONE'
+        error: 'Status should be one these: WAITING, IN_PRODUCTION, DONE.'
       });
     }
 
+    await Order.findByIdAndUpdate(orderId, { status });
+
+    res.sendStatus(204);
   } catch(error) {
     console.log(error);
     res.sendStatus(500);
--- api/src/router.ts
+++ api/src/router.ts
@@ -10,6 +10,7 @@ import { listProductsByCategory } from './app/useCases/categories/listProductsBy
 import { listOrders } from './app/useCases/orders/listOrders';
 import { createOrder } from './app/useCases/orders/createOrder';
 import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus';
+import { cancelOrder } from './app/useCases/orders/cancelOrder';
 
 export const router = Router();
 
@@ -46,9 +47,7 @@ router.get('/orders', listOrders);
 router.post('/orders', createOrder);
 
 // Change order status
-router.patch('/orders/:orderID', changeOrderStatus);
+router.patch('/orders/:orderId', changeOrderStatus);
 
 // Delete/cancel order
-router.delete('/orders/:orderID', (req, res) => {
-  res.send('OK');
-});
+router.delete('/orders/:orderId', cancelOrder);
