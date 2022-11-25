9ee1dcba7cf0b99dace52828418ec8b9fae6ed8b
anapbds <justtley@gmail.com>
2022-11-25 01:08:00 -0300

    25/11

---
+++ api/src/app/useCases/orders/changeOrderStatus.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+
+import { Order } from '../../models/Order';
+
+export async function changeOrderStatus(req: Request, res: Response) {
+  try{
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    if(['WAITING', 'IN_PRODUCTION', 'DONE'].includes(status)){
+      return res.status(400).json({
+        error: 'Status should be one these: WAITING, IN_PRODUCTION, DONE'
+      });
+    }
+
+  } catch(error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
